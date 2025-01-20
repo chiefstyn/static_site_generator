843c607838a555bdcb587af7cdddce01e03de79f
chiefstyn <aubergz@gmail.com>
2025-01-19 22:26:20 -0800

    added leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,6 +9,23 @@ class HTMLNode:
         self.props = props 
 
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+#                  <start tag : add props >            # value     # </closing tag>
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+            
+                
+
+
     def to_html(self):
         raise NotImplementedError("to_html method not implemented")
 
@@ -28,4 +45,4 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
-        
+    
